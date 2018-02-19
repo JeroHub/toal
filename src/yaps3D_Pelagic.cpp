93f5a6aa05c41048413ad44d85c8de4c13cd30f9
RTbecard <jamesadamcampbell@gmail.com>
2018-02-19 16:53:42 +0100

    Finished YAPS.  Now runs very fast
    To do:
    - Add instructions on how to estimate speed of sound

--- src/yaps3D_Pelagic.cpp
+++ src/yaps3D_Pelagic.cpp
@@ -20,42 +20,52 @@ Type objective_function<Type>::operator() ()
 
   // Fixed
   //--------------------------
-  PARAMETER(c); // Speed of sound
+  DATA_INTEGER(c); // Speed of sound
   PARAMETER(logSigma_bi);
   Type sigma_bi = exp(logSigma_bi);
 
   // Hydrophone latency distribution
-  PARAMETER_VECTOR(logSigma_dl);
-
-  // XYZ distribution
-  PARAMETER(logD_xy);    		// Log SD of XY movement/unit time
-//  PARAMETER(logD_z);        // Log SD of Z movement/unit time
+  PARAMETER(logSigma_dl);
 
   // TOA distribution
   PARAMETER(logSigma_toa); // Time of arrival SD (for each hydrophone)
-  PARAMETER(logScale_toa);		// scale-parameter for t-dist
-  PARAMETER(log_t_part);		// t-part of mixture model
+  // PARAMETER(logScale_toa);		// scale-parameter for t-dist
+  // PARAMETER(log_t_part);		// t-part of mixture model
+
+  PARAMETER(logSigma_xyz);
 
   /*****************************
   * Helper Variables
+  *
+  * Variables for temporary storage to make
+  * life easier.
+  * Try to keep to a minimum
   *****************************/
   array<Type> mu_toa(np,nh);  // Estimated toa
   array<Type> dist(np,nh);	 // dist-matrix
 
-  vector<Type> sigma_dl(nh);// SD for hydrophone latency errors
-  for(int i = 0; i < nh; ++i){
-    sigma_dl(i) = exp(logSigma_dl(i));
-  }
+  // SD for hydrophone latency errors
+  Type sigma_dl = exp(logSigma_dl);
+  // vector<Type> sigma_dl(nh);
+  // for(int i = 0; i < nh; ++i){
+  //   sigma_dl(i) = exp(logSigma_dl(i));
+  // }
 
+  // Mixture of guassian and t-distriubtions for TOA
   Type sigma_toa = exp(logSigma_toa);
-  Type D_xy = exp(logD_xy);
-//  Type D_z = exp(logD_z);
-  Type scale_toa = exp(logScale_toa);
-  Type t_part = exp(log_t_part);
-  Type G_part = Type(1.0) - t_part; // Gaussian part of mixture model
+  // Type scale_toa = exp(logScale_toa);
+  // Type t_part = exp(log_t_part);
+  // Type G_part = Type(1.0) - t_part;
 
-  // Type nll (negative log liklihood);
-  // Lower values is better fitting model
+  // Displacement
+  Type sigma_xyz = exp(logSigma_xyz);
+  Type velocity;
+
+  /***************************************************
+  * Start run
+  ***************************************************/
+  // Negative log liklihood
+  // Use the non-parallel intialization for debugging
   // Type nll = 0;
   parallel_accumulator<Type> nll(this);
 
@@ -69,7 +79,7 @@ Type objective_function<Type>::operator() ()
       if(toa(i,h) != -9999){ //ignore NA's...
         // Calculate Distance from hydrophone
         dist(i,h) = sqrt(
-          (H(h,0)-XYZ(i,0))*(H(h,0)-XYZ(i,0)) +
+            (H(h,0)-XYZ(i,0))*(H(h,0)-XYZ(i,0)) +
             (H(h,1)-XYZ(i,1))*(H(h,1)-XYZ(i,1)) +
             (H(h,2)-XYZ(i,2))*(H(h,2)-XYZ(i,2)));
 
@@ -82,25 +92,40 @@ Type objective_function<Type>::operator() ()
         // Negative log liklihood from time of arrival
         // This is a mixture of gaussian and scaled t-distribution.
         // Does the t-distribution help with smaller sample sizes?
-        nll -= log( G_part * dnorm(eps, Type(0), sigma_toa, false) + //Gaussian part
-          t_part * dt(eps/scale_toa, Type(3.0), false) / scale_toa );	//t part
+        // nll -= log( G_part * dnorm(eps, Type(0), sigma_toa, false) + //Gaussian part
+        //   t_part * dt(eps/scale_toa, Type(3.0), false) / scale_toa );	//t part
+
+        // Distribution of residuals
+        nll -= dnorm(eps, Type(0), sigma_toa, true);
       }
     }
   }
 
   /*************************************************
   * Spatial location
-  * Assumes a guassian error distribution of distances
-  * traveled along x, y, and z axes between pings.
-  * SD is scaled by inter pulse interval
+  * Assumes a gamma error distribution of swim displacement
+  * along each axis between pings.
   *************************************************/
   for(int i=1; i<np; ++i){
-    nll -= dnorm(sqrt((XYZ(i,0) - XYZ(i-1,0))*(XYZ(i,0) - XYZ(i-1,0)) +
-                      (XYZ(i,1) - XYZ(i-1,1))*(XYZ(i,1) - XYZ(i-1,1)) +
-                      (XYZ(i,2) - XYZ(i-1,2))*(XYZ(i,2) - XYZ(i-1,2))),
-                   Type(0),
-                   sqrt(2*D_xy*(top(i) - top(i-1))),
-                   true);
+    // displacement liklihood
+    nll -= dnorm(XYZ(i,0), XYZ(i-1,0),
+                 sqrt(sigma_xyz*(top(i) - top(i-1))), true);
+    nll -= dnorm(XYZ(i,1), XYZ(i-1,1),
+                 sqrt(sigma_xyz*(top(i) - top(i-1))), true);
+    nll -= dnorm(XYZ(i,2), XYZ(i-1,2),
+                 sqrt(sigma_xyz*(top(i) - top(i-1))), true);
+
+
+    // Take sqrt of dist to transfrom into a normal distribution
+    // The mean should be linearly related to the SD if x, y, and z displacement
+    // is normally distributed and centered around 0
+    // velocity = sqrt(
+    //   (XYZ(i,0) - XYZ(i-1,0))*(XYZ(i,0) - XYZ(i-1,0)) +
+    //     (XYZ(i,1) - XYZ(i-1,1))*(XYZ(i,1) - XYZ(i-1,1)) +
+    //     (XYZ(i,2) - XYZ(i-1,2))*(XYZ(i,2) - XYZ(i-1,2)))/
+    //       (top(i) - top(i-1));
+    //
+    // nll -= dnorm(sqrt(velocity), sigma_xyz*4.508148, sigma_xyz, true);
   }
 
   /*************************************************
@@ -111,7 +136,7 @@ Type objective_function<Type>::operator() ()
 
   for(int i = 0; i<np; i++){
     for(int h = 0; h<nh; h++){
-      nll -= dnorm(dl(i,h), Type(0), sigma_dl(h), true);
+      nll -= dnorm(dl(i,h), Type(0), sigma_dl, true);
     }
   }
 
@@ -123,13 +148,7 @@ Type objective_function<Type>::operator() ()
   //burst interval component
   for(int i = 2; i < np; ++i)
   {
-    if(i == 0) {
-      nll -= dnorm(top(0),Type(0.0),Type(4.0),true);
-    } else if (i == 1){
-      nll -= dnorm(top(1),Type(2.0),Type(4.0),true);
-    } else {
       nll -= dnorm(top(i)-2*top(i-1)+top(i-2), Type(0),sigma_bi, true);
-    }
   }
 
   return nll;
